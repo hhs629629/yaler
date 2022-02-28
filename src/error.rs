be0f35e686034a786f14202f756041b4f3beb950
hhs629629 <hhs629629@gmail.com>
2022-02-28 18:40:53 +0900

    Add TLS Errors

--- src/error.rs
+++ src/error.rs
@@ -5,17 +5,29 @@ pub enum Error {
     #[error("Fail to bind tcp listener")]
     TcpBindError(std::io::Error),
 
-    #[error("Fail to accept client")]
+    #[error("Fail to accept client with tcp")]
     TcpAcceptError(std::io::Error),
 
-    #[error("Fail to connect remote")]
+    #[error("Fail to connect remote with tcp")]
     TcpConnectError(std::io::Error),
+
+    #[error("Fail to accept client with tls")]
+    TlsAcceptError(std::io::Error),
+
+    #[error("Fail to connect remote with tls")]
+    TlsConnectError(std::io::Error),
 
     #[error("Invalid http request")]
     BadHttpError(std::io::Error),
 
     #[error("tokio::io::AsyncReadExt::read_until error")]
     ReadUntilError(std::io::Error),
+
+    #[error("Stream returned error")]
+    ReadStreamError(std::io::Error),
+
+    #[error("Stream returned error")]
+    WriteStreamError(std::io::Error),
 
     #[error("Fail to parse http")]
     HttpParseError(#[from] pext::FromUtf8Err),
